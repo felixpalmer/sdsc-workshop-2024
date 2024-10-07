7a60be0963d2b830908dd4ffdb0174350c94a144
felixpalmer <felixpalmer@gmail.com>
2024-10-07 10:45:19 +0200

    Better tip blending

--- glowing-arc-layer.ts
+++ glowing-arc-layer.ts
@@ -1,5 +1,22 @@
 import { ArcLayer, ArcLayerProps } from '@deck.gl/layers';
 
+// export type BlendFactor =
+// 125   | 'zero'
+// 126   | 'one'
+// 127   | 'src-color'
+// 128   | 'one-minus-src-color'
+// 129   | 'src-alpha'
+// 130   | 'one-minus-src-alpha'
+// 131   | 'dst-color'
+// 132   | 'one-minus-dst-color'
+// 133   | 'dst-alpha'
+// 134   | 'one-minus-dst-alpha'
+// 135   | 'src-alpha-saturated'
+// 136   | 'constant-color'
+// 137   | 'one-minus-constant-color'
+// 138   | 'constant-alpha'
+// 139   | 'one-minus-constant-alpha';
+
 export const ADDITIVE_BLEND_PARAMETERS = {
   blendColorSrcFactor: 'src-alpha',
   blendAlphaSrcFactor: 'src-alpha',
@@ -31,6 +48,9 @@ export default class GlowingArcLayer extends ArcLayer {
       fragColor.rgb = mix(vec3(1.0), fragColor.rgb, glow - along);
       fragColor.rgb = mix(fragColor.rgb, vec3(0.0), 0.5 * join);
 
+      // Tip fading (avoid artifact from overlapping blend)
+      float tipFade = smoothstep(0.07, 0.1, geometry.uv.x) + smoothstep(1.0, 0.99, geometry.uv.x) - 1.0;
+
       // Gaussian blur edge for glow
       float sigma = 1.0 / 3.0;
       float a = -0.5 / (sigma * sigma);
@@ -38,7 +58,11 @@ export default class GlowingArcLayer extends ArcLayer {
       float t = fromCenter - along;
       float weight = w0 * exp(a * t * t);   
       fragColor.a *= weight;
-      fragColor.a = mix(1.0, 0.5 * fragColor.a, smoothstep(0.2, 0.3, fromCenter));
+      fragColor.a = mix(1.0, 0.5 * fragColor.a * tipFade, smoothstep(0.2, 0.3, fromCenter));
+
+      if (fragColor.a < 0.01) {
+        discard;
+      }
       `,
     };
 
