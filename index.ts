7a6f8def0b3b846596be5a8395a86f7fd6720acc
felixpalmer <felixpalmer@gmail.com>
2024-10-09 11:48:11 +0200

    Enable first example

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import './style.css';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
-//import { initialize } from "./example1";
+// Uncomment specific example to run
+import { initialize } from "./example1";
 //import { initialize } from "./example2";
 //import { initialize } from "./example3";
 //import { initialize } from "./example4";
 //import { initialize } from "./example5";
 //import { initialize } from "./example6";
-import { initialize } from "./example7";
+//import { initialize } from "./example7";
 
 initialize();
